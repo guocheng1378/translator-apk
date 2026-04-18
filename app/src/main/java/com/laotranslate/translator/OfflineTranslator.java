652900b8b271d21b9ebcc4021b869a70e314d4ea
guocheng1378 <guocheng1378@users.noreply.github.com>
2026-04-19 03:58:12 +0800

    fix: use createTensor(env, Object) for bool tensor - shape auto-inferred

--- app/src/main/java/com/laotranslate/translator/OfflineTranslator.java
+++ app/src/main/java/com/laotranslate/translator/OfflineTranslator.java
@@ -311,7 +311,7 @@ public class OfflineTranslator {
                 decInputs.put("encoder_hidden_states", encoderHiddenStates);
                 decInputs.put("input_ids", decInputTensor);
                 // 添加 use_cache_branch 输入 (NLLB-200 模型需要)
-                OnnxTensor useCacheBranchTensor = OnnxTensor.createTensor(env, new boolean[]{false}, new long[]{1});
+                OnnxTensor useCacheBranchTensor = OnnxTensor.createTensor(env, new boolean[]{false});
                 decInputs.put("use_cache_branch", useCacheBranchTensor);
 
                 result = decoderSession.run(decInputs);
@@ -328,7 +328,7 @@ public class OfflineTranslator {
                 inputs.put("input_ids", inputTensor);
                 inputs.put("attention_mask", maskTensor);
                 // 添加 use_cache_branch 输入 (NLLB-200 模型需要)
-                OnnxTensor useCacheBranchTensor2 = OnnxTensor.createTensor(env, new boolean[]{false}, new long[]{1});
+                OnnxTensor useCacheBranchTensor2 = OnnxTensor.createTensor(env, new boolean[]{false});
                 inputs.put("use_cache_branch", useCacheBranchTensor2);
 
                 result = decoderSession.run(inputs);
